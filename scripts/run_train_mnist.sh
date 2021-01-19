bfc1df667f50cc2b568683408d19fef6c19a15c5
da-li1 <da.li1@samsung.com>
2021-01-19 19:08:30 +0000

    change the code path to avoid permission problem

--- scripts/run_train_mnist.sh
+++ scripts/run_train_mnist.sh
@@ -3,8 +3,6 @@
 export EXPR_ID=1111
 export DATA_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/data'
 export CHECKPOINT_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/ckpt'
-export CODE_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE'
-cd CODE_DIR
 python train.py --data $DATA_DIR/mnist --root $CHECKPOINT_DIR --save $EXPR_ID --dataset mnist --batch_size 200 \
         --epochs 400 --num_latent_scales 2 --num_groups_per_scale 10 --num_postprocess_cells 3 --num_preprocess_cells 3 \
         --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 --num_latent_per_group 20 --num_preprocess_blocks 2 \
