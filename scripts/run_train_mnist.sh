cf5657eb1aaa41b857e385ca09b411e4ee3a5f5f
da-li1 <da.li1@samsung.com>
2021-01-19 18:31:02 +0000

    mnist train script

---
+++ scripts/run_train_mnist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export EXPR_ID=1111
+export DATA_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/data'
+export CHECKPOINT_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/ckpt'
+export CODE_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE'
+cd CODE_DIR
+python train.py --data $DATA_DIR/mnist --root $CHECKPOINT_DIR --save $EXPR_ID --dataset mnist --batch_size 200 \
+        --epochs 400 --num_latent_scales 2 --num_groups_per_scale 10 --num_postprocess_cells 3 --num_preprocess_cells 3 \
+        --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 --num_latent_per_group 20 --num_preprocess_blocks 2 \
+        --num_postprocess_blocks 2 --weight_decay_norm 1e-2 --num_channels_enc 32 --num_channels_dec 32 --num_nf 0 \
+        --ada_groups --num_process_per_node 2 --use_se --res_dist --fast_adamax
