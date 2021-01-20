f4d5cabe00600882286f81e0afb21f846a95e795
da-li1 <da.li1@samsung.com>
2021-01-20 21:39:27 +0000

    use python

--- scripts/setup_imagenet_data.sh
+++ scripts/setup_imagenet_data.sh
@@ -6,5 +6,5 @@ cd $DATA_DIR/imagenet-oord
 wget https://storage.googleapis.com/glow-demo/data/imagenet-oord-tfr.tar
 tar -xvf imagenet-oord-tfr.tar
 
-python3 convert_tfrecord_to_lmdb.py --dataset=imagenet-oord_32 --tfr_path=$DATA_DIR/imagenet-oord/mnt/host/imagenet-oord-tfr --lmdb_path=$DATA_DIR/imagenet-oord/imagenet-oord-lmdb_32 --split=train
-python3 convert_tfrecord_to_lmdb.py --dataset=imagenet-oord_32 --tfr_path=$DATA_DIR/imagenet-oord/mnt/host/imagenet-oord-tfr --lmdb_path=$DATA_DIR/imagenet-oord/imagenet-oord-lmdb_32 --split=validation
+python convert_tfrecord_to_lmdb.py --dataset=imagenet-oord_32 --tfr_path=$DATA_DIR/imagenet-oord/mnt/host/imagenet-oord-tfr --lmdb_path=$DATA_DIR/imagenet-oord/imagenet-oord-lmdb_32 --split=train
+python convert_tfrecord_to_lmdb.py --dataset=imagenet-oord_32 --tfr_path=$DATA_DIR/imagenet-oord/mnt/host/imagenet-oord-tfr --lmdb_path=$DATA_DIR/imagenet-oord/imagenet-oord-lmdb_32 --split=validation
