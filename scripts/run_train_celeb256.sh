14c678804e562ac41d34ba26a4580617086c3204
da-li1 <da.li1@samsung.com>
2021-01-21 22:34:09 +0000

    convert data

---
+++ scripts/run_train_celeb256.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export EXPR_ID=0121
+export DATA_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/data'
+export CHECKPOINT_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/ckpt'
+export CODE_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/'
+export IP_ADDR='127.0.0.1'
+export NODE_RANK=2
+cd $CODE_DIR
+mpirun --allow-run-as-root -np 3 -npernode 1 bash -c \
+        'python train.py --data $DATA_DIR/celeba/celeba-lmdb --root $CHECKPOINT_DIR --save $EXPR_ID --dataset celeba_256 \
+        --num_channels_enc 30 --num_channels_dec 30 --epochs 300 --num_postprocess_cells 2 --num_preprocess_cells 2 \
+        --num_latent_scales 5 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
+        --num_preprocess_blocks 1 --num_postprocess_blocks 1 --weight_decay_norm 1e-2 --num_groups_per_scale 16 \
+        --batch_size 4 --num_nf 2 --ada_groups --min_groups_per_scale 4 \
+        --weight_decay_norm_anneal --weight_decay_norm_init 1. --num_process_per_node 8 --use_se --res_dist \
+        --fast_adamax --num_x_bits 5 --node_rank $NODE_RANK --num_proc_node 3 --master_address $IP_ADDR '
