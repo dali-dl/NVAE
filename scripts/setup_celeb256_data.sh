c07b1905d7ac0df5a68489baf04460da3996894c
da-li1 <da.li1@samsung.com>
2021-01-21 22:23:59 +0000

    convert data

--- scripts/setup_celeb256_data.sh
+++ scripts/setup_celeb256_data.sh
@@ -2,10 +2,10 @@
 
 export DATA_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/data'
 export CODE_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/'
-mkdir -p $DATA_DIR/celeba
-cd $DATA_DIR/celeba
-wget https://storage.googleapis.com/glow-demo/data/celeba-tfr.tar
-tar -xvf celeba-tfr.tar
+#mkdir -p $DATA_DIR/celeba
+#cd $DATA_DIR/celeba
+#wget https://storage.googleapis.com/glow-demo/data/celeba-tfr.tar
+#tar -xvf celeba-tfr.tar
 cd $CODE_DIR/scripts
-python convert_tfrecord_to_lmdb.py --dataset=celeba --tfr_path=$DATA_DIR/celeba/celeba-tfr --lmdb_path=$DATA_DIR/celeba/celeba-lmdb --split=train
-python convert_tfrecord_to_lmdb.py --dataset=celeba --tfr_path=$DATA_DIR/celeba/celeba-tfr --lmdb_path=$DATA_DIR/celeba/celeba-lmdb --split=validation
+python3 convert_tfrecord_to_lmdb.py --dataset=celeba --tfr_path=$DATA_DIR/celeba/celeba-tfr --lmdb_path=$DATA_DIR/celeba/celeba-lmdb --split=train
+python3 convert_tfrecord_to_lmdb.py --dataset=celeba --tfr_path=$DATA_DIR/celeba/celeba-tfr --lmdb_path=$DATA_DIR/celeba/celeba-lmdb --split=validation
