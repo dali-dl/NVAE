6764fb62c26a770eb47dbf7b1377864a99df219b
da-li1 <da.li1@samsung.com>
2021-01-22 11:38:00 +0000

    set up celeb 64

--- scripts/setup_celeb64_data.sh
+++ scripts/setup_celeb64_data.sh
@@ -2,6 +2,6 @@
 
 export CODE_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/'
 cd $CODE_DIR/scripts
-python create_celeba64_lmdb.py --split train --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
-python create_celeba64_lmdb.py --split valid --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
-python create_celeba64_lmdb.py --split test  --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
+python3 create_celeba64_lmdb.py --split train --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
+python3 create_celeba64_lmdb.py --split valid --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
+python3 create_celeba64_lmdb.py --split test  --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
