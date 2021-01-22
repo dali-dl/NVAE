622289b3d7bef12287238014bca505cc5f5afe1f
da-li1 <da.li1@samsung.com>
2021-01-22 11:55:49 +0000

    change path of celeba path

--- scripts/setup_celeb64_data.sh
+++ scripts/setup_celeb64_data.sh
@@ -3,6 +3,6 @@
 export DATA_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/data'
 export CODE_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/'
 cd $CODE_DIR/scripts
-python create_celeba64_lmdb.py --split train --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
-python create_celeba64_lmdb.py --split valid --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
-python create_celeba64_lmdb.py --split test  --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
+python3 create_celeba64_lmdb.py --split train --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
+python3 create_celeba64_lmdb.py --split valid --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
+python3 create_celeba64_lmdb.py --split test  --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
