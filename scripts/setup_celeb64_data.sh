cfff1c2430aaaee6ff025c26e51c91829b9f7adb
da-li1 <da.li1@samsung.com>
2021-01-22 11:58:36 +0000

    change path of celeba path

--- scripts/setup_celeb64_data.sh
+++ scripts/setup_celeb64_data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export http_proxy="http://75.12.251.5:8080"
+export https_proxy="http://75.12.251.5:8080"
+export ftp_proxy="ftp://75.12.251.5:8080"
+export no_proxy="127.0.0.1, localhost, 192.168.20.45, 75.17.101.8, 75.17.101.5, 75.17.101.45"
+
 export DATA_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/data'
 export CODE_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/'
 cd $CODE_DIR/scripts
