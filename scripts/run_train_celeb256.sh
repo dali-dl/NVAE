0dba84629dac98956028e2f93adbb4c055c09201
da-li1 <da.li1@samsung.com>
2021-01-22 13:55:26 +0000

    run on two for mnist and debug celeba

--- scripts/run_train_celeb256.sh
+++ scripts/run_train_celeb256.sh
@@ -10,5 +10,5 @@ python3 train.py --data $DATA_DIR/celeba/celeba-lmdb --root $CHECKPOINT_DIR --sa
         --num_latent_scales 5 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --weight_decay_norm 1e-2 --num_groups_per_scale 16 \
         --batch_size 4 --num_nf 2 --ada_groups --min_groups_per_scale 4 \
-        --weight_decay_norm_anneal --weight_decay_norm_init 1. --num_process_per_node 4 --use_se --res_dist \
+        --weight_decay_norm_anneal --weight_decay_norm_init 1. --num_process_per_node 1 --use_se --res_dist \
         --fast_adamax --num_x_bits 5
