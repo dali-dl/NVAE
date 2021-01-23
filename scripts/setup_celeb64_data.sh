af4b6fa61293c63c18ed271d20e84cb5042c69d1
da-li1 <da.li1@samsung.com>
2021-01-23 15:29:34 +0000

    setup celeb 64

--- scripts/setup_celeb64_data.sh
+++ scripts/setup_celeb64_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export http_proxy="http://75.12.251.5:8080"
-export https_proxy="http://75.12.251.5:8080"
-export ftp_proxy="ftp://75.12.251.5:8080"
-export no_proxy="127.0.0.1, localhost, 192.168.20.45, 75.17.101.8, 75.17.101.5, 75.17.101.45"
+#export http_proxy="http://75.12.251.5:8080"
+#export https_proxy="http://75.12.251.5:8080"
+#export ftp_proxy="ftp://75.12.251.5:8080"
+#export no_proxy="127.0.0.1, localhost, 192.168.20.45, 75.17.101.8, 75.17.101.5, 75.17.101.45"
 
-export DATA_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/data'
-export CODE_DIR='/group-volume/orc_sruk/users/da.li1/git/NVAE/'
+export DATA_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/data'
+export CODE_DIR='/group-volume/ASR-Unlabeled-Data/users/da.li1/git/NVAE/'
 cd $CODE_DIR/scripts
 python3 create_celeba64_lmdb.py --split train --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
 python3 create_celeba64_lmdb.py --split valid --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
